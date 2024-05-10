60eeb26c05a829d5a49413d0c87fa39c31455b6f
Wai Yong Tong <waiyongtong@gmail.com>
2024-05-10 13:35:12 +0800

    add weather data retrieval functionality and integrate geolocation and timezone handling

--- weatherforecast.py
+++ weatherforecast.py
@@ -12,3 +12,30 @@ root.title("Weather App")
 rootgeometry=root.geometry("500x650+300+200")
 root.resizable(False,False)
 
+def getweather():
+    try:
+        city=textfield.get()
+
+        geolocator=Nominatim(user_agent="geoapiExercises")
+        location=geolocator.geocode(city)   
+        obj=TimezoneFinder()
+        result=obj.timezone_at(lng=location.longitude,lat=location.latitude)
+        print(result)
+
+        home=pytz.timezone(result)
+        local_time=datetime.now(home)
+        current_time=local_time.strftime("%I:%M %p")
+        clock.config(text=current_time)
+        name.config(text="CURRENT WEATHER")
+
+        #weather
+        api="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=27d637bf8af0772aa37a30a5702df3ae"
+
+        json_data=requests.get(api).json()
+        print(json_data)
+        condition=json_data['weather'][0]['main']
+        description =json_data['weather'][0]['description']
+        temp= int(json_data['main']['temp']-273.15)
+        pressure =json_data['main']['pressure']
+        humidity =json_data['main']['humidity']
+        wind =json_data['wind']['speed']
