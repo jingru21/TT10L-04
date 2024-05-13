88e25731b9e8bafd9d549d63d894e6b7a6cc547a
Wai Yong Tong <waiyongtong@gmail.com>
2024-05-13 21:21:16 +0800

    add login frame from line 20 to 27 and left side image from line 31 to 35

--- loginpage.py
+++ loginpage.py
@@ -10,11 +10,26 @@ class LoginPage:
         self.window.state('zoomed')
         self.window.title('Login Page')
 
-        # ========================================================================
-        # ============================background image============================
-        # ========================================================================
+        # background image
         self.bg_frame = Image.open(r"C:\calendar\images\background1.png")
         photo = ImageTk.PhotoImage(self.bg_frame)
         self.bg_panel = Label(self.window, image=photo)
         self.bg_panel.image = photo
         self.bg_panel.pack(fill='both', expand='yes')
+        # Login Frame
+        self.lgn_frame = Frame(self.window, bg='#040405', width=950, height=600)
+        self.lgn_frame.place(x=200, y=70)
+        self.txt = "WELCOME"
+        self.heading = Label(self.lgn_frame, text=self.txt, font=('yu gothic ui', 25, "bold"), bg="#040405",
+                             fg='white',
+                             bd=5,
+                             relief=FLAT)
+        self.heading.place(x=80, y=30, width=300, height=30)
+
+       
+        # Left Side Image
+        self.side_image = Image.open(r"C:\calendar\images\vector.png")
+        photo = ImageTk.PhotoImage(self.side_image)
+        self.side_image_label = Label(self.lgn_frame, image=photo, bg='#040405')
+        self.side_image_label.image = photo
+        self.side_image_label.place(x=5, y=100)
