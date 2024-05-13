501975f640f2bf4af612541a166430ad43f33a57
Wai Yong Tong <waiyongtong@gmail.com>
2024-05-13 21:13:10 +0800

    setup background size and background image

---
+++ loginpage.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+
+class LoginPage:
+    def __init__(self, window):
+        self.window = window
+        self.window.geometry('1166x718')
+        self.window.resizable(0, 0)
+        self.window.state('zoomed')
+        self.window.title('Login Page')
+
+        # ========================================================================
+        # ============================background image============================
+        # ========================================================================
+        self.bg_frame = Image.open(r"C:\calendar\images\background1.png")
+        photo = ImageTk.PhotoImage(self.bg_frame)
+        self.bg_panel = Label(self.window, image=photo)
+        self.bg_panel.image = photo
+        self.bg_panel.pack(fill='both', expand='yes')
