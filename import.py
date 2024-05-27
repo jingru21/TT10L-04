15f345d2edc716fd72043e3f023456f2dbe0719a
Ong Jing Ru <ongjingru2005@gmail.com>
2024-05-28 03:01:36 +0800

    import other file

--- import.py
+++ import.py
@@ -11,8 +11,11 @@ win.minsize(width=400, height=200)
 # pinned
 win.attributes("-topmost", 1)
 
-label = Label(win, text="Welcome to Almanac!!!", font=("Helvetica", 30))
-label.pack(pady=20)
+#bgcolor
+win.config(bg="#797EF6")
+
+label = Label(win, text="Welcome to Almanac!!!", font=("Comic Sans MS", 50), bg="#797EF6", fg="white")
+label.pack(pady=20, side="top")
 
 def open_calendar():
     os.system('python C:\\CSP1123\\GitProject\\TT10L-04\\mainInterface.py')
@@ -24,14 +27,14 @@ def open_weather():
     os.system('python C:\\CSP1123\GitProject\\TT10L-04\\weatherforecast.py')
 
 import_button = [
-    Button(win,text="Calendar", command=open_calendar),
-    Button(win,text="Weather Forecast", command=open_weather),
-    Button(win,text="Clock", command=open_clock),
-    Button(win,text="Exit", command=win.destroy)
+    Button(win,text="Calendar", command=open_calendar,bg="lightblue", fg="black", font=("Comic Sans MS", 12),width=20, height=2),
+    Button(win,text="Weather Forecast", command=open_weather,bg="lightblue", fg="black", font=("Comic Sans MS", 12),width=20, height=2),
+    Button(win,text="Clock", command=open_clock, bg="lightblue", fg="black", font=("Comic Sans MS", 12),width=20, height=2),
+    Button(win,text="Exit", command=win.destroy, bg="lightblue", fg="black", font=("Comic Sans MS", 12),width=20, height=2)
 ]
 
 
 for button in import_button:
-    button.pack(pady=2)
+    button.pack(pady=2, anchor="center")
 
 win.mainloop()
