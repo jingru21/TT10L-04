e712a3a44ded474c2ec8384f7b76fab58b98c006
Ong Jing Ru <ongjingru2005@gmail.com>
2024-06-04 01:08:06 +0800

    delete some extra words

--- mainInterface.py
+++ mainInterface.py
@@ -41,41 +41,20 @@ color_theme.add_command(label='Pink Theme', command=lambda: change_theme("pink")
 def change_theme(theme):
     if theme == "light":
         bg_color="white"
-<<<<<<< HEAD
-<<<<<<< HEAD
         button_bg = "#EDEDE9"
         button_fg = "black"
         update_calendar_colors("#F5EBE0", "#D5BDAF", "#E3D5CA", "#D6CCC2", "#E3D5CA", "black", "black")
-=======
         button_bg = "#FFB6C1"
         button_fg = "black"
         update_calendar_colors("#FFB6C1", "#FF69B4", "#FF1493", "#FFB6C1", "#DC143C", "#FFB6C1", "#FFA07A")
->>>>>>> a4b810de8a8ebff762473d3e19af35ff4950393d
-=======
         button_bg = "#f7f5bc"
         button_fg = "black"
-        update_calendar_colors("#ece75f", "#f7f5bc", "#f1ee8e", "#ece75f", "#e6cc00", "#e47200", "black")
-<<<<<<< HEAD
->>>>>>> 15f345d2edc716fd72043e3f023456f2dbe0719a
-=======
->>>>>>> 63d78e157367e8f3f0b880c9d5a7b49fd3e1f2fc
->>>>>>> a4b810de8a8ebff762473d3e19af35ff4950393d
+        update_calendar_colors("#ece75f", "#f7f5bc", "#f1ee8e", "#ece75f", "#e6cc00", "#e47200","black")
     elif theme == "dark":
         bg_color="#1a1625"
         button_bg = "#282828"
         button_fg = "white"
-<<<<<<< HEAD
-<<<<<<< HEAD
         update_calendar_colors("#282828", "#3f3f3f", "#8b8b8b", "#46424f", "#717171", "black", "black")
-=======
-        update_calendar_colors("#282828", "#3f3f3f", "#8b8b8b", "#717171", "#46424f", "#121212", "black")
->>>>>>> 15f345d2edc716fd72043e3f023456f2dbe0719a
-=======
-        update_calendar_colors("#282828", "#3f3f3f", "#8b8b8b", "#46424f", "#717171", "#121212", "#5e5a66")
-=======
-        update_calendar_colors("#282828", "#3f3f3f", "#8b8b8b", "#717171", "#46424f", "#121212", "black")
->>>>>>> 63d78e157367e8f3f0b880c9d5a7b49fd3e1f2fc
->>>>>>> a4b810de8a8ebff762473d3e19af35ff4950393d
     elif theme == "blue":
         bg_color="#ADD8E6"
         button_bg = "#87CEEB"
@@ -90,7 +69,7 @@ def change_theme(theme):
         bg_color="#FFD1D7"
         button_bg = "#FFB6C1"
         button_fg = "black"
-        update_calendar_colors("#FFB6C1", "#FF99AC", "#FF69B4", "#F7CAD0", "#FF477E", "black", "black")
+        update_calendar_colors("#FFB6C1", "#FF99AC", "#FF69B4", "#F7CAD0", "#FF477E", "black","black")
     
     win.config(bg=bg_color)
     frame.config(bg=bg_color) 
